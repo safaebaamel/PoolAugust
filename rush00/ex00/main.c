0c6e66aa3b1b080ee8d68585f321d9031c996758
sapae <57509402+sapae@users.noreply.github.com>
2020-05-22 05:12:58 +0000

    new updates

---
+++ rush00/ex00/main.c
@@ -0,0 +1,8 @@
+
+void	rush(int a, int b);
+
+int	main()
+{
+	rush(1, 5);
+	return 0;
+}
---
+++ rush00/ex00/rush00.c
@@ -0,0 +1,24 @@
+
+void	ft_putchar(char c);
+
+void	ft_printing(char left, char midd, char right, int size)
+{
+	ft_putchar(left);
+	while(size-- > 2)
+		ft_putchar(midd);
+	if (size == 1)
+		ft_putchar(right);
+	ft_putchar('\n');
+}
+
+
+void	rush(int a, int b)
+{
+	if (a < 1 || b < 1)
+		return ;
+	ft_printing('o', '-', 'o', a);
+	while (b-- > 2)
+		ft_printing('|', ' ', '|', a);
+	if (b == 1)
+		ft_printing('o', '-', 'o', a);
+}
